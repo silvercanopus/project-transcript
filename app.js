18b726d46bcfe7933d4ad9da0628cdc840a9d62e
silvercanopus <76857954+silvercanopus@users.noreply.github.com>
2022-10-26 22:56:15 -0400

    Add helpful comments to app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Import libraries
 const express = require('express');
 const methodOverride = require('method-override'); 
 const ejsMate = require('ejs-mate');
@@ -7,15 +8,19 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 
+// Import utility functions
 const catchAsync = require('./utils/catchAsync');
 const AppError = require('./utils/AppError');
 
+// Import routes
 const userRoutes = require('./routes/users');
 const scriptRoutes = require('./routes/scripts');
 const translationRoutes = require('./routes/translations');
 
+// Import the User DB (required to enable authentication)
 const User = require('./models/user');
 
+// Connect to the database
 const mongoose = require('mongoose');
 const MongoDBStore = require('connect-mongo');
 const dbUrl = 'mongodb://localhost:27017/project-transcript';
@@ -27,6 +32,7 @@ db.once('open', () => {
     console.log("Database Connected!");
 })
 
+// Initialize the app
 const app = express();
 
 app.engine('ejs', ejsMate);
@@ -38,6 +44,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Enable sessions
 const secret = 'ThisShouldBeABetterSecret';
 
 const store = MongoDBStore.create({
@@ -45,7 +52,6 @@ const store = MongoDBStore.create({
     secret,
     touchAfter: 24 * 60 * 60
 })
-
 store.on('error', function (e) {
     console.log("Session Store Error", e);
 })
@@ -64,12 +70,14 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig));
 
+// Enable Passport and authentication
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Enable Flash
 app.use(flash());
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
@@ -78,24 +86,27 @@ app.use((req, res, next) => {
     next();
 })
 
+// Start listening on localhost port 3000 (change this when deploying)
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000")
 })
 
+// Set the home page
 app.get('/', (req, res) => {
     res.render('home');
 })
 
+// Set the various routes
 app.use('/', userRoutes);
-
 app.use('/', scriptRoutes);
-
 app.use('/', translationRoutes);
 
+// For catching all undefined routes
 app.all('*', (req, res, next) => {
     next(new AppError(404, "Page Not Found"));
 })
 
+// Error-handling for the front-end
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = "Oops, something went wrong";
